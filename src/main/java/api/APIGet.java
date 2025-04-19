ddcf97a926c48edc86bc5c91f42e40c3137c56a5
Timur <timur.schmidt@student.maastrichtuniversity.nl>
2025-04-19 21:45:07 +0200

    Changed Java built in HTTP to spring http implementation. Added Method to query cards by set.

--- src/main/java/api/APIGet.java
+++ src/main/java/api/APIGet.java
@@ -1,19 +1,15 @@
 package api;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import data.Card;
 import io.github.cdimascio.dotenv.Dotenv;
 import lombok.NoArgsConstructor;
 import lombok.extern.java.Log;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 import util.APICache;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.logging.Level;
@@ -22,16 +18,15 @@ import java.util.logging.Level;
 @NoArgsConstructor
 public class APIGet {
     private static final String API_BASE_URL = "https://api.pokemontcg.io/v2/";
+    /* unused */
     private static final String CARD_URL = "https://api.pokemontcg.io/v2/cards/";
-    private static final ObjectMapper objectMapper = createObjectMapper();
+    private static final String CARD_ENDPOINT = "/cards/{cardId}";
     private static final String apiKey = getSecret();
-    private static final HttpClient client = HttpClient.newHttpClient();
-
-    private static ObjectMapper createObjectMapper() {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        return mapper;
-    }
+    private static final WebClient client = WebClient.builder()
+            .baseUrl(API_BASE_URL)
+            .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .defaultHeader("X-Api-Key", apiKey)
+            .build();
 
     private static String getSecret() {
         Dotenv dotenv = null;
@@ -43,100 +38,74 @@ public class APIGet {
         }
     }
 
-
     public static CompletableFuture<Card> getCardDataById(String cardId) {
-        // Check if the card is in the cache
         Card cachedCard = APICache.getCardById(cardId);
         if (cachedCard != null) {
-            log.info("Card found in cache: " + cardId);
-            CompletableFuture<Card> future = new CompletableFuture<>();
-            future.complete(cachedCard);
-            return future;
+            return CompletableFuture.completedFuture(cachedCard);
         }
-
-        try {
-            URI targetURI = new URI(CARD_URL + cardId);
-            HttpRequest request = HttpRequest.newBuilder().uri(targetURI).header("X-Api-Key", apiKey).GET().build();
-            return client.sendAsync(request, HttpResponse.BodyHandlers.ofString()).thenApply(response -> {
-                if (response.statusCode() == 200) {
-                    try {
-                        ResponseWrapper wrapper = objectMapper.readValue(response.body(), ResponseWrapper.class);
-                        Card card = wrapper.getData();
-
-                        // Cache the card data
-                        if (card != null) {
-                            APICache.putCard(cardId, card);
-                        }
-
-                        return card;
-                    } catch (JsonProcessingException e) {
-                        log.log(Level.SEVERE, "Error parsing JSON response: {0}", e.getMessage());
-                        return null;
+        Mono<Card> cardMono = client.get().uri(CARD_ENDPOINT, cardId).retrieve()
+                .bodyToMono(ResponseWrapper.class).map(ResponseWrapper::getData)
+                .doOnSuccess(card -> {
+                    if (card != null) {
+                        APICache.putCard(cardId, card);
+                        log.info("Successfully fetched card with ID: " + cardId);
+                    } else {
+                        log.warning("Fetched card data was null for ID: " + cardId);
                     }
-                } else {
-                    log.log(Level.SEVERE, "Error! GET request returned {0} instead of 200!", response.statusCode());
-                    return null;
-                }
-            }).exceptionally(e -> {
-                log.log(Level.SEVERE, "Error sending HTTP request: {0}!", e.getMessage());
-                return null;
-            });
-        } catch (URISyntaxException e) {
-            log.log(Level.SEVERE, "Error creating URI: {0}", e.getMessage());
-            CompletableFuture<Card> future = new CompletableFuture<>();
-            future.complete(null);
-            return future;
-        }
+                }).doOnError(error -> log.log(Level.SEVERE, "Error fetching card data: {0}", error.getMessage()))
+                .onErrorResume(error -> {
+                    log.log(Level.SEVERE, "API call failed: {0}!", error.getMessage());
+                    return Mono.empty();
+                });
+        return cardMono.toFuture();
     }
 
     public static CompletableFuture<List<Card>> queryCardsByName(String name) {
-        // Check if the search results are in the cache
         List<Card> cachedResults = APICache.getSearchResults(name);
         if (cachedResults != null) {
-            log.info("Search results found in cache for: " + name);
-            CompletableFuture<List<Card>> future = new CompletableFuture<>();
-            future.complete(cachedResults);
-            return future;
+            return CompletableFuture.completedFuture(cachedResults);
         }
-
-        String uriPart = "name:" + name;
-        String fullUriString = CARD_URL + "?q=" + uriPart;
-        try {
-            URI uri = new URI(fullUriString);
-            HttpRequest request = HttpRequest.newBuilder().uri(uri).header("X-Api-Key", apiKey).GET().build();
-            return client.sendAsync(request, HttpResponse.BodyHandlers.ofString()).thenApply(response -> {
-                if (response.statusCode() == 200) {
-                    try {
-                        String responseBody = response.body();
-                        double sizeInMB = (double) responseBody.getBytes().length / (1024 * 1024);
-                        log.info(String.format("Response for query '%s': %.2f MB", name, sizeInMB));
-                        ListResponseWrapper wrapper = objectMapper.readValue(response.body(), ListResponseWrapper.class);
-                        List<Card> results = wrapper.getData();
-
-                        // Cache the search results
-                        if (results != null) {
-                            APICache.putSearchResults(name, results);
-                        }
-
-                        log.info("Request processed");
-                        return results;
-                    } catch (JsonProcessingException e) {
-                        log.log(Level.SEVERE, "Error parsing JSON response: {0}", e.getMessage());
-                        return null;
+        String queryParam = "name:" + name;
+        Mono<List<Card>> cardMono = client.get().uri(uriBuilder -> uriBuilder.path("/cards")
+                        .queryParam("q", queryParam).build()).retrieve()
+                .bodyToMono(ListResponseWrapper.class).map(ListResponseWrapper::getData)
+                .doOnSuccess(results -> {
+                    if (results != null) {
+                        APICache.putSearchResults(name, results);
+                        log.info(String.format("Successfully fetched %s cards for query '%s'", results.size(), name));
+                    } else {
+                        log.warning("Fetched card list was null for query: " + name);
                     }
-                } else {
-                    log.log(Level.SEVERE, "Error! GET requested returned {0} instead of 200!", response.statusCode());
-                    return null;
-                }
-            }).exceptionally(e -> {
-                log.log(Level.SEVERE, "Error sending HTTP request: {0}", e.getMessage());
-                return null;
-            });
-        } catch (URISyntaxException e) {
-            log.log(Level.SEVERE, "Error in creating URI: {0}", e.getMessage());
-            CompletableFuture<List<Card>> future = new CompletableFuture<>();
-            future.complete(null);
-            return future;
+                }).doOnError(error -> log.log(Level.SEVERE, "Error querying for cards: {0}", error.getMessage()))
+                .onErrorResume(e -> {
+                    log.log(Level.SEVERE, "API call failed for query: {0}", e.getMessage());
+                    return Mono.empty();
+                });
+        return cardMono.toFuture();
+    }
+
+    public static CompletableFuture<List<Card>> queryCardsBySet(String set) {
+        List<Card> cachedResults = APICache.getSearchResults(set);
+        if (cachedResults != null) {
+            return CompletableFuture.completedFuture(cachedResults);
         }
+        String queryString = "set.id:" + set;
+        Mono<List<Card>> cardMono = client.get().uri(uriBuilder -> uriBuilder.path("/cards")
+                        .queryParam("q", queryString).build()).retrieve()
+                .bodyToMono(ListResponseWrapper.class).map(ListResponseWrapper::getData)
+                .doOnSuccess(results -> {
+                    if (results != null) {
+                        APICache.putSearchResults(set, results);
+                        log.info(String.format("Successfully fetched %s cards or query '%s'", results.size(), set));
+                    } else {
+                        log.warning("Fetched card list was null for query: " + set);
+                    }
+                }).doOnError(e -> log.log(Level.SEVERE, "Error querying for cards: {0}", e.getMessage()))
+                .onErrorResume(e -> {
+                    log.log(Level.SEVERE, "API call failed for query: {0}", e.getMessage());
+                    return Mono.empty();
+                });
+        return cardMono.toFuture();
     }
 }
+
